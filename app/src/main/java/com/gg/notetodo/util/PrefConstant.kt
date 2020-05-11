87ff84aecfea1f9f292251b5121fefe2f6e9e557
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-11 21:37:51 +0530

    Create StoreSession.kt

---
+++ app/src/main/java/com/gg/notetodo/util/PrefConstant.kt
@@ -0,0 +1,9 @@
+package com.gg.notetodo.util
+
+object PrefConstant {
+    const val SHARED_PREFERENCE_NAME = "note_todo_app_pref"
+    const val IS_LOGGED_IN = "is_logged_in"
+    const val FULL_NAME = "full_name"
+    const val USER_NAME = "user_name"
+    const val PASSWORD = "password"
+}
