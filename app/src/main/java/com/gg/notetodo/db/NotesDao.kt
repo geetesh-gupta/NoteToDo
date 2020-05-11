04466c438caba4907e2259c9255e473f710b6e1d
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 00:58:20 +0530

    Create NotesDao.kt

---
+++ app/src/main/java/com/gg/notetodo/db/NotesDao.kt
@@ -0,0 +1,24 @@
+package com.gg.notetodo.db
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+
+@Dao
+interface NotesDao {
+
+    @Query("SELECT * from todoData")
+    fun getAll(): List<Notes>
+
+    @Insert(onConflict = REPLACE)
+    fun insert(notes: Notes)
+
+    @Update
+    fun updateNotes(notes: Notes)
+
+    @Delete
+    fun delete(notes: Notes)
+
+    @Query("DELETE FROM todoData WHERE isTaskCompleted = :status")
+    fun deleteNotes(status: Boolean)
+
+}
