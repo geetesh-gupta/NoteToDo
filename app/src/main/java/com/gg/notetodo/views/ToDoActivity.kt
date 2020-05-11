656fb7d538bfb7c633f37a5bf0bb0d13aa50fe48
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-11 21:45:03 +0530

    Create ToDoActivity

---
+++ app/src/main/java/com/gg/notetodo/views/ToDoActivity.kt
@@ -0,0 +1,13 @@
+package com.gg.notetodo.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gg.notetodo.R
+
+class ToDoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do)
+    }
+}
