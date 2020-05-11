602f824015964150c24f09a62efa61ae3bf6ed09
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 00:58:10 +0530

    Create Notes.kt

---
+++ app/src/main/java/com/gg/notetodo/db/Notes.kt
@@ -0,0 +1,20 @@
+package com.gg.notetodo.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "todoData")
+data class Notes(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ColumnInfo(name = "title")
+    var title: String = "",
+    @ColumnInfo(name = "description")
+    var description: String = "",
+    @ColumnInfo(name = "imagePath")
+    var imagePath: String = "",
+    @ColumnInfo(name = "isTaskCompleted")
+    var isTaskCompleted: Boolean = false
+)
